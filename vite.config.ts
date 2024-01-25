653f3050af6283c28740895b84c6f87c0fa30c49
Sylvain.D <60609924+JerkStanton@users.noreply.github.com>
2024-01-25 13:18:48 +0100

    sttings

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default {
   base: './', // Use relative paths
   build: {
     outDir: 'dist',
     assetsDir: './', // Use relative paths
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`, // Use predictable file names
+        chunkFileNames: `[name].js`, // Use predictable file names
+        assetFileNames: `[name].[ext]` // Use predictable file names
+      }
+    }
   }
 }
